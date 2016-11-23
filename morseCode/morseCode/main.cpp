f06fc3fa58e5827884ffba117157339953c87c94
Randy McMillan <randy.lee.mcmillan@gmail.com>
2016-11-23 10:10:52 -0500

    git add * ; uncrustify; git commit

---
+++ morseCode/morseCode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  morseCode
+//
+//  Created by Randy McMillan on 11/23/16.
+//  Copyright © 2016 RandyMcMillan.net. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
