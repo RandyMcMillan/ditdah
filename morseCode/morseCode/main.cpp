5d8394ff13f65d2c0aaea93e20fea8d6dde46c9e
Randy McMillan <randy.lee.mcmillan@gmail.com>
2016-11-23 10:53:28 -0500

    use maps to structure morse data

--- morseCode/morseCode/main.cpp
+++ morseCode/morseCode/main.cpp
@@ -7,6 +7,20 @@
 //
 
 #include <iostream>
+#include <algorithm>    // std::make_heap, std::pop_heap, std::push_heap, std::sort_heap
+#include <vector>       // std::vector
+#include <string>
+
+
+//http://www.cplusplus.com/reference/map/map/map/
+//http://en.cppreference.com/w/cpp/container/map/find
+
+
+std::string dah[] = {"-","--","---","----","-----"};
+std::vector<std::string> v(dah,dah+5);
+
+
+
 
 int main(int argc, const char * argv[]) {
     // insert code here...
