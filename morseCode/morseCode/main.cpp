4cd96dba0e9de840f5401b080b8938fc9c304566
Randy McMillan <randy.lee.mcmillan@gmail.com>
2016-11-23 14:12:39 -0500

    git add * ; uncrustify; git commit

--- morseCode/morseCode/main.cpp
+++ morseCode/morseCode/main.cpp
@@ -16,31 +16,57 @@ int main()
     else
         cout << "Found";
     
-    
-    
     typedef map<string,string> DitDah;
-    DitDah dit;
-    dit.insert(make_pair("_","T"));
-    dit.insert(make_pair("__","M"));
-    dit.insert(make_pair("___","O"));
-    dit.insert(make_pair("____","?"));
-    dit.insert(make_pair("_____","0"));
+    DitDah ditdah;
 //
-    dit.insert(make_pair("_.","N"));
-    dit.insert(make_pair("_..","D"));
-    dit.insert(make_pair("_...","B"));
-    dit.insert(make_pair("_....","6"));
+    ditdah.insert(make_pair("-","T"));
+    ditdah.insert(make_pair("--","M"));
+    ditdah.insert(make_pair("---","O"));
+    ditdah.insert(make_pair("----","?"));
+    ditdah.insert(make_pair("-----","0"));
 //
-    dit.insert(make_pair("_._","K"));
-    dit.insert(make_pair("_.__","Y"));
-    dit.insert(make_pair("_._.","C"));
+    ditdah.insert(make_pair("-.","N"));
+    ditdah.insert(make_pair("-..","D"));
+    ditdah.insert(make_pair("-...","B"));
+    ditdah.insert(make_pair("-....","6"));
 //
-    dit.insert(make_pair("_..","D"));
-    dit.insert(make_pair("_.._","X"));
+    ditdah.insert(make_pair("-.-","K"));
+    ditdah.insert(make_pair("-.--","Y"));
+    ditdah.insert(make_pair("-.-.","C"));
 //
-    dit.insert(make_pair("_...","B"));
-    dit.insert(make_pair("_....","6"));
+    ditdah.insert(make_pair("-..","D"));
+    ditdah.insert(make_pair("-..-","X"));
+//
+    ditdah.insert(make_pair("-...","B"));
+    ditdah.insert(make_pair("-....","6"));
+//
     
-    
+    ditdah.insert(make_pair(".","E"));
+    ditdah.insert(make_pair("..","I"));
+    ditdah.insert(make_pair("...","S"));
+    ditdah.insert(make_pair("....","H"));
+    ditdah.insert(make_pair(".....","5"));
+//
+    ditdah.insert(make_pair(".-","A"));
+    ditdah.insert(make_pair(".--","W"));
+    ditdah.insert(make_pair(".---","J"));
+    ditdah.insert(make_pair(".----","1"));
+//
+    ditdah.insert(make_pair("..","I"));
+    ditdah.insert(make_pair("..-","U"));
+    ditdah.insert(make_pair("..--","-"));
+    ditdah.insert(make_pair("..---","2"));
+//
+    ditdah.insert(make_pair("-..","D"));
+    ditdah.insert(make_pair("-..-","X"));
+//
+    ditdah.insert(make_pair("..-.","F"));
+    ditdah.insert(make_pair("...","S"));
+    ditdah.insert(make_pair("...-","V"));
+    ditdah.insert(make_pair("...--","3"));
+//
+    ditdah.insert(make_pair("....","H"));
+    ditdah.insert(make_pair("....-","4"));
+    ditdah.insert(make_pair(".....","5"));
     
 }
