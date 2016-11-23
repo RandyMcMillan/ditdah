8973d2b97fc980e87eb203e81c701f10d9b9f059
Randy McMillan <randy.lee.mcmillan@gmail.com>
2016-11-23 13:51:00 -0500

    git add * ; uncrustify; git commit

--- morseCode/morseCode/main.cpp
+++ morseCode/morseCode/main.cpp
@@ -1,63 +1,31 @@
-//Program tested on Microsoft Visual Studio 2008 - Zahid Ghadialy
-//This program shows use of multi-maps
-//We input multiple phone numbers for different people
-#include<iostream>
-#include<map>
+#include <map>
+#include <iostream>
 #include <string>
-
 using namespace std;
-
-//forward declaration
-void printer(multimap<string, int> pN);
-
+//http://stackoverflow.com/questions/8404260/stl-mapchar-char-destructor
 int main()
 {
-    multimap<string, int> phoneNums;
+    typedef map<string,string> Strings;
+    Strings strings;
+    strings.insert(make_pair("foo","bar"));
     
-    //Insert key, value as pairs
-    phoneNums.insert(pair<string, int>("Joe",123));
-    phoneNums.insert(pair<string, int>("Will",444));
-    printer(phoneNums);
+    string ss = "foo";
+    Strings::iterator it = strings.find(ss);
+    if( it == strings.end() )
+        cout << "Not Found~!";
+    else
+        cout << "Found";
     
-    //Insert duplicates
-    phoneNums.insert(pair<string, int>("Joe",369));
-    phoneNums.insert(pair<string, int>("Smith",567));
-    phoneNums.insert(pair<string, int>("Joe",888));
-    phoneNums.insert(pair<string, int>("Will",999));
-    printer(phoneNums);
     
-    //Checking frequency of different keys
-    cout<<"\n\nFrequency of different names"<<endl;
-    cout<<"Number of Phones for Joe = "<<phoneNums.count("Joe")<<endl;
-    cout<<"Number of Phones for Will = "<<phoneNums.count("Will")<<endl;
-    cout<<"Number of Phones for Smith = "<<phoneNums.count("Smith")<<endl;
-    cout<<"Number of Phones for Zahid = "<<phoneNums.count("Zahid")<<endl;
     
-    //Print all Joe from the list and then erase them
-    pair<multimap<string,int>::iterator, multimap<string,int>::iterator> ii;
-    multimap<string, int>::iterator it; //Iterator to be used along with ii
-    ii = phoneNums.equal_range("Joe"); //We get the first and last entry in ii;
-    cout<<"\n\nPrinting all Joe and then erasing them"<<endl;
-    for(it = ii.first; it != ii.second; ++it)
-    {
-        cout<<"Key = "<<it->first<<"    Value = "<<it->second<<endl;
-    }
-    phoneNums.erase(ii.first, ii.second);
-    printer(phoneNums);
+    typedef map<string,string> DitDah;
+    DitDah dit;
+    dit.insert(make_pair("_","T"));
     
-    return 0;
+    
+    
+    
+    
+    
+    
 }
-
-//This method prints the vector
-void printer(multimap<string, int> pN)
-{
-    cout<<"\n\nMultimap printer method"<<endl;
-    cout<<"Map size = "<<pN.size()<<endl;
-    multimap<string, int>::iterator it = pN.begin();
-    while(it != pN.end())
-    {
-        cout<<"Key = "<<it->first<<"    Value = "<<it->second<<endl;
-        it++;
-    }
-}
-
