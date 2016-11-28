34d9e1298429733f9bb3bcf7fef6eef3351e3917
Randy McMillan <randy.lee.mcmillan@gmail.com>
2016-11-28 09:01:37 -0500

    git add * ; uncrustify; git commit

--- tests.sh
+++ tests.sh
@@ -1,11 +1,54 @@
 #~/ditdah/ditdah/DerivedData/ditdah/Build/Products/Debug/./morseCode
 #!/bin/bash
-
 echo A = 
 ./ditdah/DerivedData/ditdah/Build/Products/Debug/./ditdah A
-
 echo B = 
 ./ditdah/DerivedData/ditdah/Build/Products/Debug/./ditdah B
-
 echo C = 
 ./ditdah/DerivedData/ditdah/Build/Products/Debug/./ditdah C
+echo D = 
+./ditdah/DerivedData/ditdah/Build/Products/Debug/./ditdah D
+echo E = 
+./ditdah/DerivedData/ditdah/Build/Products/Debug/./ditdah E
+echo F = 
+./ditdah/DerivedData/ditdah/Build/Products/Debug/./ditdah F
+echo G = 
+./ditdah/DerivedData/ditdah/Build/Products/Debug/./ditdah G
+echo H = 
+./ditdah/DerivedData/ditdah/Build/Products/Debug/./ditdah H
+echo I = 
+./ditdah/DerivedData/ditdah/Build/Products/Debug/./ditdah I
+echo J = 
+./ditdah/DerivedData/ditdah/Build/Products/Debug/./ditdah J
+echo K = 
+./ditdah/DerivedData/ditdah/Build/Products/Debug/./ditdah K
+echo L = 
+./ditdah/DerivedData/ditdah/Build/Products/Debug/./ditdah L
+echo M = 
+./ditdah/DerivedData/ditdah/Build/Products/Debug/./ditdah M
+echo N = 
+./ditdah/DerivedData/ditdah/Build/Products/Debug/./ditdah N
+echo O = 
+./ditdah/DerivedData/ditdah/Build/Products/Debug/./ditdah O
+echo P = 
+./ditdah/DerivedData/ditdah/Build/Products/Debug/./ditdah P
+echo Q = 
+./ditdah/DerivedData/ditdah/Build/Products/Debug/./ditdah Q
+echo R = 
+./ditdah/DerivedData/ditdah/Build/Products/Debug/./ditdah R
+echo S = 
+./ditdah/DerivedData/ditdah/Build/Products/Debug/./ditdah S
+echo T = 
+./ditdah/DerivedData/ditdah/Build/Products/Debug/./ditdah T
+echo U = 
+./ditdah/DerivedData/ditdah/Build/Products/Debug/./ditdah U
+echo V = 
+./ditdah/DerivedData/ditdah/Build/Products/Debug/./ditdah V
+echo W = 
+./ditdah/DerivedData/ditdah/Build/Products/Debug/./ditdah W
+echo X = 
+./ditdah/DerivedData/ditdah/Build/Products/Debug/./ditdah X
+echo Y = 
+./ditdah/DerivedData/ditdah/Build/Products/Debug/./ditdah Y
+echo Z = 
+./ditdah/DerivedData/ditdah/Build/Products/Debug/./ditdah Z
